4e3eea69308107db260dfcaf14e89eb9cb74d237
kareem.shaik <kareem.shaik@GGKU5DELL1279>
2023-03-07 10:24:23 +0530

    Athentication Changes

---
+++ AngularAuthAPI/AngularAuthAPI/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿namespace AngularAuthAPI.Controllers
+{
+    using AngularAuthAPI.Context;
+    using AngularAuthAPI.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+
+       [HttpPost("authenticate")]
+       public async Task<IActionResult> Authenticate([FromBody] User userobj)
+       {
+            if (userobj == null)
+                return BadRequest();
+            //User is not null check the Db first
+            var user = await _authContext.Users
+                .FirstOrDefaultAsync(x => x.UserName == userobj.UserName && x.Password == userobj.Password);
+            if(user == null)
+                return NotFound(new {Message = "User Not Found!"});
+
+            return Ok(new
+            {
+                Message = "Login Success!"
+            });
+       }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if(userObj == null)
+                return BadRequest();
+
+            //Add in db
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new 
+            {
+                Message = "User is Registered!"
+            });
+
+        }
+
+    }
+}
--- AngularAuthAPI/AngularAuthAPI/Program.cs
+++ AngularAuthAPI/AngularAuthAPI/Program.cs
@@ -1,3 +1,6 @@
+using AngularAuthAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("MyPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlServerConnStr"));
+});
 
 var app = builder.Build();
 
@@ -18,6 +34,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("MyPolicy");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
